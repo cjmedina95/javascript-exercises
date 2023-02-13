64c89bf184d274c7c90a5e93b7e3493d055f989a
Christian <Christian.Medina.ATX@gmail.com>
2023-02-12 23:44:57 -0600

    Finish exercises 1 - 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,5 @@
-const repeatString = function() {
-
+const repeatString = function(word, count) {
+    return count >= 0 ? word.repeat(count) : 'ERROR';
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayList, item) {
+    for (let i=1; i < arguments.length; i++) {
+        arrayList = arrayList.filter(item => item in arrayList);
+        console.log(arguments[i]);
+        console.log(arrayList);
+    }
 };
 
 // Do not edit below this line
