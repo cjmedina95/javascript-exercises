c772fa24a59b990fdf5891dc1f0484934a65c905
Christian Medina <117231596+cjmedina95@users.noreply.github.com>
2023-02-13 10:08:37 -0600

    Add comments to code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,3 +1,9 @@
+// Accepts a string and splits it into an array of characters.
+// Then uses the reverse() function to reorder the array.
+// Finally, rejoins the reversed array. join('') and split('')
+// use empty strings to avoid including commas in
+// the returned string.
+
 const reverseString = function(str) {
     return str.split('').reverse().join('');
 };
