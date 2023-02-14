e0e2cec9887f2c3854f0c1207a451333778d3c1d
Christian <Christian.Medina.ATX@gmail.com>
2023-02-13 20:33:52 -0600

    Finish exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,15 @@
-const removeFromArray = function(arrayList, item) {
+// Loops through an indefinite length list of arguments,
+// then checks arrayList for whether it includes that argument.
+// If so, the function calls splice() and continues to the next argument.
+
+const removeFromArray = function(arrayList, args) {
     for (let i=1; i < arguments.length; i++) {
-        arrayList = arrayList.filter(item => item in arrayList);
-        console.log(arguments[i]);
-        console.log(arrayList);
+        if (arrayList.includes(arguments[i])) {
+            arrayList.splice(arrayList.indexOf(arguments[i]), 1);
+        }
     }
+
+    return arrayList;
 };
 
 // Do not edit below this line
