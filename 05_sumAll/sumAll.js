b768630798778ba7d1288f46a8cbda23b5de9257
Christian <Christian.Medina.ATX@gmail.com>
2023-02-13 21:04:45 -0600

    Finish exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,37 @@
-const sumAll = function() {
+function addNums(priorSum, currentNumber) {
+    return priorSum + currentNumber;
+}
 
+const sumAll = function(bound1, bound2) {
+    if(!Number.isInteger(bound1) || !Number.isInteger(bound2)) {
+        return "ERROR";
+    }
+
+    if (bound1 < 0 || bound2 < 0) {
+        return "ERROR";
+    }
+
+    let numberList = [];
+    let sum = 0;
+    if (bound1 > bound2) {
+        var upperBound = bound1; // var instead of let because of block scoping.
+        var lowerBound = bound2;
+    } 
+    else if (bound2 > bound1) {
+        var upperBound = bound2;
+        var lowerBound = bound1;
+    }
+    else {
+        return sum; // When both bounds are equal, the result is the original number.
+    }
+
+    for (lowerBound; lowerBound <= upperBound; lowerBound++) {
+        numberList.push(lowerBound);
+    }
+
+    sum = numberList.reduce(addNums)
+
+    return sum;
 };
 
 // Do not edit below this line
