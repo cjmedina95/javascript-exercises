3a7b6f8aaaafdf0389355b5b5b75804adf0fda03
Christian <Christian.Medina.ATX@gmail.com>
2023-02-13 21:26:14 -0600

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+// Uses standard formulae for converting between celsius and fahrenheit.
+// Because toFixed() includes trailing zeroes, a + to return the numeric
+// value of the object is required to remove any unneeded decimal values.
+const convertToCelsius = function(temp) {
+  return +((temp - 32) * (5 / 9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return +((temp * (9/5)) + 32).toFixed(1);
 };
 
 // Do not edit below this line
