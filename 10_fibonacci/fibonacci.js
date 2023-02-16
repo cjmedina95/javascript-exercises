6cd9f22a9b6f64ab852087eabb07fc9dc39405a0
Christian <Christian.Medina.ATX@gmail.com>
2023-02-15 23:19:47 -0600

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+// Fibonacci function defined at https://en.wikipedia.org/wiki/Fibonacci_number
+// 
+const fibonacci = function(endPos) {
+    if (endPos < 0) {
+        return "OOPS";
+    }
 
+    const phi = 1.618033988749895; // As represented in Math.js
+    const golden = Math.pow(phi, endPos);
+    const divisor = Math.sqrt(5);
+    return(Math.round(golden / divisor));
 };
 
 // Do not edit below this line
