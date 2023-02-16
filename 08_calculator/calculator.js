53be57e83d350425fca55b16282099ae18088f42
Christian <Christian.Medina.ATX@gmail.com>
2023-02-15 19:11:48 -0600

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.length == 0 ? 0 : numbers.reduce(add);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce(function(num1, num2) {
+    return num1 * num2;
+  });
 };
 
-const power = function() {
-	
+const power = function(num1, exponent) {
+	return Math.pow(num1, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+	if (num1 === 1 || num1 === 0) {
+    return 1;
+  }
+  return num1 * factorial(num1 - 1);
 };
 
 // Do not edit below this line
