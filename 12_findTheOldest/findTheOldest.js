1e28491aa19e37ec47562a8f6ac677bcd71789e6
Christian <Christian.Medina.ATX@gmail.com>
2023-02-17 19:07:10 -0600

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleList) {
+    const dateObj = new Date();
+    const currentYear = dateObj.getFullYear();
+    let oldestPerson = {};
+    let oldestAge = 0;
 
+    peopleList.forEach(function(person) {
+        let personAge = 0;
+    
+        if (!Object.keys(person).includes("yearOfDeath")) {
+            personAge = currentYear - person.yearOfBirth;
+        }
+        else {
+            personAge = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (personAge > oldestAge) {
+            oldestPerson = person;
+            oldestAge = personAge;
+        }     
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
