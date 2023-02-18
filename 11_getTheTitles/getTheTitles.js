57e056a5b229efcd9ebd849b9a11a4b9b3b5f72d
Christian <Christian.Medina.ATX@gmail.com>
2023-02-17 18:39:08 -0600

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+// Maps the title object to a new array, then returns the array.
+const getTheTitles = function(books) {
+    const titles = books.map(({title}) => (title));
+    return titles;
 };
 
 // Do not edit below this line
